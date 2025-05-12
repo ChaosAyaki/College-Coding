3366c2303d2b5415baf4fa79bf7f4bdcb180bbba
Ayaki <chengye25@gmail.com>
2025-05-12 08:46:16 +0200

    probando el nuevo tema

---
+++ First Year/F.Info 2/code/Tema7/Apuntes/PI.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<iomanip> //Nueva libreria del tema
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    cout << fixed;
+    cout << setprecision(5) << M_PI << endl;
+    return 0;
+}
