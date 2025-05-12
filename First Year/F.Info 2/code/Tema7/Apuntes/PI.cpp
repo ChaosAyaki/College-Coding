84951649302974c5ce6c781f4d5bc17d56d4aa09
Ayaki <chengye25@gmail.com>
2025-05-12 08:53:16 +0200

    terminado

--- First Year/F.Info 2/code/Tema7/Apuntes/PI.cpp
+++ First Year/F.Info 2/code/Tema7/Apuntes/PI.cpp
@@ -5,7 +5,10 @@
 using namespace std;
 
 int main(){
-    cout << fixed;
-    cout << setprecision(5) << M_PI << endl;
+    float a;
+    a = M_PI; //Porque me daba pereza poner "M_PI" todo el rato 
+    cout << fixed; //Esto permite que el siguiente numero que salga pueda ser un decimal 
+    cout << setprecision(5) << M_PI << endl; //Lo que hace precision es limitar el numero de decimales 
+    cout << fixed << setprecision(22) << a << endl; //Se puede poner asi tambien
     return 0;
 }
